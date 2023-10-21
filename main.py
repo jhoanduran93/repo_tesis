04f12ac7624af1b2965dd3accc9b4f664eed4641
jhoanduran93 <jhoanduran933@gmail.com>
2023-10-21 14:45:25 -0300

    prueba 2

--- main.py
+++ main.py
@@ -91,10 +91,9 @@ async def chatbot_endpoint(websocket: WebSocket):
 
                     # Extrae la respuesta generada por GPT-3
                     answer = response.choices[0].text
-                    if not answer.strip():
-                        logger.debug(f"Respuesta al cliente: {answer}")
-                        await websocket.send_text(answer)
-                        continue  # Saltar al siguiente ciclo
+                    
+                    logger.debug(f"Respuesta al cliente: {answer}")
+                    await websocket.send_text(answer)
 
                 except Exception as e:
                     await websocket.send_text(f"Error en la generación de respuesta: {str(e)}")
