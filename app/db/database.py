8a703f118aea0abb21d8601b86eb8955ea95cf57
jhoanduran93 <jhoanduran933@gmail.com>
2023-10-25 21:05:10 -0300

    modifica

--- app/db/database.py
+++ app/db/database.py
@@ -1,13 +1,25 @@
 import mysql.connector
 from mysql.connector import errorcode
 
+def reconnect_to_database(db_config):
+    while True:
+        try:
+            # Intenta reconectar a la base de datos
+            conn = mysql.connector.connect(**db_config)
+            print("Reconexión exitosa.")
+            return conn
+        except mysql.connector.Error as err:
+            print(f"Error de reconexión: {err}")
+            continue
+
 # Configura la conexión a la base de datos
 db_config = {
-    "host": "bls0lv8b15zncfgr7bde-mysql.services.clever-cloud.com",  
+    "host": "bls0lv8b15zncfgr7bde-mysql.services.clever-cloud.com",
     "user": "u2i1vzpgkh9zegil",
     "password": "RTfrtrFKOovCPvgjMi84",
-    "database": "bls0lv8b15zncfgr7bde",  
+    "database": "bls0lv8b15zncfgr7bde",
 }
+
 try:
     # Intenta establecer la conexión
     conn = mysql.connector.connect(**db_config)
@@ -19,3 +31,6 @@ except mysql.connector.Error as err:
         print("Error: La base de datos no existe.")
     else:
         print(f"Error: {err}")
+    
+    # Intenta la reconexión automática en caso de error de conexión
+    conn = reconnect_to_database(db_config)
